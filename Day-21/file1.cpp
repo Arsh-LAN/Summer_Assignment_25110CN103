fd1b465a8b5c5d0015e625ed440a0b1d18b55028
Arsh-LAN <arshlanali6110@gmail.com>
2026-06-24 22:08:40 +0530

    Day-21 assignment has been uploaded

---
+++ Day-21/file1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    int length = 0;
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    while (str[length] != '\0')
+    {
+        length++;
+    }
+
+    cout << "Length of the string = " << length;
+
+    return 0;
+}
